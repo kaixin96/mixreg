e8bae0533a47c6b207d3979cef6cedfdf097265a
kaixin96 <kaixin.wang@u.nus.edu>
2020-10-20 10:54:48 +0800

    remove scripts

--- experiments/rainbow_cutout_color/starpilot.sh
+++
@@ -1,3 +0,0 @@
-mpiexec -np 9 python -m train_procgen.train_dqn --env_name starpilot --num_levels 500 --data_aug cutout_color --test_worker_interval 9 --run_id 321 --gpus_id 0,1,2
-mpiexec -np 9 python -m train_procgen.train_dqn --env_name starpilot --num_levels 500 --data_aug cutout_color --test_worker_interval 9 --run_id 322 --gpus_id 0,1,2
-mpiexec -np 9 python -m train_procgen.train_dqn --env_name starpilot --num_levels 500 --data_aug cutout_color --test_worker_interval 9 --run_id 323 --gpus_id 0,1,2
